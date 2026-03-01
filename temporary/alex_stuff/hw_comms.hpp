8b0bb3764942bea5a859b9f9f4d1283f419a8d0e
alexjiang03 <113951549+alexjiang03@users.noreply.github.com>
2026-03-01 17:54:30 -0500

    Create hw_comms.hpp
    Added fingerprint sensor constants
    ## STILL NEED TO TEST ##

---
+++ temporary/alex_stuff/hw_comms.hpp
@@ -0,0 +1,56 @@
+#ifndef HW_COMMS_HPP
+#define HW_COMMS_HPP
+
+#include <string>
+#include "errors.hpp"
+#include <map>
+
+#define DEBUG_SIMPLE_DISCOVERY_MODE // Uncomment this to use simplified discovery for VM work
+
+typedef enum {
+    UNKNOWN_DEVICE_CMD = 0,
+    HANDSHAKE_HELLO,
+    GET_SERIAL_NUMBER,
+    GET_FIRMWARE_VERSION,
+    GET_PRIVATE_KEY
+} deviceCmd;
+
+/**
+ * @brief Maps from command names to the string that we actually send to the USB key
+ * to implement that command
+ */ 
+const std::map<deviceCmd, std::string> deviceCmdMap = {
+    {HANDSHAKE_HELLO, "pc_hello\n"},
+    {GET_SERIAL_NUMBER, "pc_req_sn\n"},
+    {GET_FIRMWARE_VERSION, "pc_req_fw\n"},
+    {GET_PRIVATE_KEY, "pc_req_key\n"},
+    {ADD_FINGERPRINT, "pc_enroll_fingerprint\n"},
+    {AUTH_FINGERPRINT, "pc_authenticate_fingerprint\n"}
+};
+
+/**
+ * @brief Translates deviceCmd value to appropriate string to be sent to device
+ */
+std::string deviceCmdToStr (const deviceCmd cmd) {
+    auto it = deviceCmdMap.find(cmd);
+    if (it != deviceCmdMap.end()) {
+        return it->second;
+    }
+    return "unknown_command\n";
+}
+
+deviceErr initDeviceComms(struct sp_port **port);
+deviceErr getNewSerialPort(std::string &portName);
+void trimTrailingNewlines(std::string& s);
+void trimTrailingNewlines(char *s);
+deviceErr sendToKey(deviceCmd cmd, struct sp_port *port);
+deviceErr readFromKey(std::string &msg, struct sp_port *port);
+
+deviceErr performHandshake(struct sp_port *port);
+deviceErr getSerialNumber(std::string &sn, struct sp_port *port);
+deviceErr getFirmware(std::string &fw, struct sp_port *port);
+deviceErr getPrivateKey(std::string &key, struct sp_port *port);
+deviceErr addFingerprint(std::string &resp, struct sp_port *port);
+deviceErr authFingerprint(std::string &resp, struct sp_port *port);
+
+#endif
