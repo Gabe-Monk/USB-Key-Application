8704d4c3b4769bc25b6caa9521977d8810ddb137
Gabriel Monk <gabemonk81@gmail.com>
2026-03-08 19:04:17 -0400

    Updated entrypoint.sh to run hw_comms in loop, so that device can be reconnected successfully

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-/app/build/bin/hw_comms &
+# Run hw_comms in a loop so it restarts if it crashes
+(while true; do /app/build/bin/hw_comms; sleep 1; done) &
 pid=$!
 
 # exec helps forward SIGTERM from a "docker stop" to the python program to help
